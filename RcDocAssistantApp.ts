479bf62f05f977c78090dcdbaac1b59a5c41a305
samir0607 <samir.gupta987@gmail.com>
2025-04-01 11:28:45 +0530

    Add logging functionality and update author contact information in app.json

--- RcDocAssistantApp.ts
+++ RcDocAssistantApp.ts
@@ -6,7 +6,10 @@ import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 
 export class RcDocAssistantApp extends App {
+    private readonly appLogger: ILogger
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+        this.appLogger = this.getLogger();
+        this.appLogger.debug('Hello, World!')
     }
 }
