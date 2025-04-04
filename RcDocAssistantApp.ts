c9ca49676b19ae96f4fd020d69e2106c58d17746
samir0607 <samir.gupta987@gmail.com>
2025-04-04 12:33:17 +0530

    feat: added settings for model choice

--- RcDocAssistantApp.ts
+++ RcDocAssistantApp.ts
@@ -6,15 +6,18 @@ import {
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { NewCommand } from './commands/NewCommand';
+import { settings } from './settings/settings';
 
 export class RcDocAssistantApp extends App {
-    private readonly appLogger: ILogger
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
-        this.appLogger = this.getLogger();
-        this.appLogger.debug('Hello, World!')
     }
     public async extendConfiguration(configuration: IConfigurationExtend) {
-        configuration.slashCommands.provideSlashCommand(new NewCommand());
+        await Promise.all([
+            ...settings.map((settings) => 
+                configuration.settings.provideSetting(settings)
+            ),
+            configuration.slashCommands.provideSlashCommand(new NewCommand()),
+        ]);
     }
 }
